2a251fd5712e94cb74e165ed740dfb44cdad846c
A.J. Romaniello <ajiellodev@gmail.com>
2020-12-18 18:11:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,19 +1,7 @@
 function iterativeLog(arr) {
-  idx = 0
-  arr.forEach((elem) => {
-    console.log(`${idx}: ${arr[idx]}`);
+  idx = 0;
+  arr.forEach(elem => {
+    console.log(`${idx}: ${elem}`);
     idx++;
   })
-}
-
-function iterate(call) {
-  arr = [" ", " ", " "]
-
-  arr.forEach(call);
-
-  return arr;
-}
-
-function doToArray(arr, call) {
-  arr.forEach(call);
 }
